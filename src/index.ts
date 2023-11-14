95ef2822e5c2a33fe4fc225242911e4faa97a1cd
Ivan Kirilenko <i.kirilenko@pulkovo-service.ru>
2023-11-14 19:22:12 +0300

    add Dockerfile docker-compose.yml

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,6 @@ import { basicAuthConfig, serverConfig } from './config';
 import { getSwaggerFile } from './utils';
 
 (async () => {
-    logger.debug(JSON.stringify(new Date()));
-
     try {
         logger.debug(`The server is start, pid#${process.pid}, ts#${Date.now()}`);
         process.on('exit', (code) => logger.debug(`The server is stop, code#${code} pid#${process.pid}, ts#${Date.now()}`));
